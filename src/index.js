f4f6cef39f36b0e7bece352a435ddb238e768967
Kostya Gorodnichuk <coolkostya@gmail.com>
2020-02-27 01:51:40 +0300

    feat: Add array check

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array){return 0}
+  var n=array[0];
+  array.forEach(element => {
+    if(element<n){n=element}
+  });
+  return array.length==0?0:n;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array){return 0}
+  var n=array[0];
+  array.forEach(element => {
+    if(element>n){n=element}
+  });
+  return array.length==0?0:n;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array){return 0}
+  var n=0;
+  array.forEach(element=>{n+=element});
+  return array.length==0?0:n/array.length;
 }
